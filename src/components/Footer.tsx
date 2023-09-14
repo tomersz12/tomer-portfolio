70e623d70a41b42ec7b0610dca9cfabf761c75d2
tszul056 <tomeruchisz@gmail.com>
2023-09-14 02:10:52 -0400

    changed links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
-import EnvelopeSVG from '../images/envelope.svg';
-import PaperclipSVG from '../images/paperclip.svg';
-import GitHubSVG from '../images/github.svg';
-import LinkedInSVG from '../images/linkedin.svg';
-import YoutueSVG from '../images/youtube.svg';
+import EmailIcon from '@mui/icons-material/Email';
+import DescriptionIcon from '@mui/icons-material/Description';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 import Resume from '../files/TomerSzulszteinResume_Sep07-2023.pdf';
 
@@ -12,20 +12,20 @@ const Footer = () => {
             <div className="footer">
                 <div className="footer-content">
                     <div className="footer-links">
-                        <a href={'mailto:tomeruchisz@gmail.com'} target="_blank">
-                            <img src={EnvelopeSVG} className="icon" alt="Email Me"/>
+                        <a href={'mailto:tomeruchisz@gmail.com'} title="Email Me" target="_blank">
+                            <EmailIcon className="icon"/>
                         </a>
                         <a href={Resume} target="_blank">
-                            <img src={PaperclipSVG} className="icon" alt="Resume"/>
+                            <DescriptionIcon className="icon"/>
                         </a>
                         <a href="https://github.com/tomersz12" target="_blank">
-                            <img src={GitHubSVG} className="icon" alt="GitHub"/>
+                            <GitHubIcon className="icon"/>
                         </a>
                         <a href="https://www.linkedin.com/in/tomersz12/" target="_blank">
-                            <img src={LinkedInSVG} className="icon" alt="LinkedIn"/>
+                            <LinkedInIcon className="icon"/>
                         </a>
                         <a href="https://www.youtube.com/@dexil12" target="_blank">
-                            <img src={YoutueSVG} className="icon" alt="YouTube"/>
+                            <YouTubeIcon className="icon"/>
                         </a>
                     </div>
                     <p>&copy; 2023 Tomer Szulsztein</p>

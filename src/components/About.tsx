9cfa0861c19c7e00974ab7fbfa24426896009fc0
tszul056 <tomeruchisz@gmail.com>
2024-02-20 20:35:07 -0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,9 +14,9 @@ const About = () => {
                         <p>
                         Welcome to my website! 
                         I'm a creative problem solver with a passion for exploring out-of-the-box solutions. 
-                        Engaging in diverse interests and hobbies fuels my imagination, and I derive joy from witnessing ideas come to life, be they mine or someone else's.
+                        I engage in diverse interests and hobbies, and I derive joy from witnessing ideas come to life, be they mine or someone else's.
                         <br/><br/>
-                        This will be a space for me to share my thoughts and experiences, aiming to create a platform that reflects my genuine enthusiasm for my various interests and a celebration of the art of creation. 
+                        This space will serve as a platform to share my thoughts and experiences, reflecting my enthusiasm for my various interests and a celebration of the art of creation. 
                         Whether it's discussing the latest movies, expressing my thoughts through writing, or sharing my travel experiences, I'm excited to have a place to do so.
                         <br/><br/>
                         Currently, I work as a software developer and am pursuing a degree in Computer Science. 

5191abfb4aa356c02649e0b43e65c9243a2988a0
tszul056 <tomeruchisz@gmail.com>
2024-02-21 20:22:22 -0500

    fix

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -5,13 +5,14 @@ import '../scss/blog.scss';
 import HomeIcon from '@mui/icons-material/Home';
 
 const Blog = () => {
+
     return (
         <>
         <div className="blog-home">
             <a href={'/'} title="Home" className='icons'>
                 <HomeIcon className="icon"/>
             </a>
-            <ArticlePreviewer id={articles[articles.length]}/>
+            <ArticlePreviewer id={articles[articles.length-1]}/>
         </div>
         <Footer/>
         </>

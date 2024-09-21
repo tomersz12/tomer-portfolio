0c07c439174ae2c18134d54404a2876e5dc21f59
tszul056 <tomeruchisz@gmail.com>
2024-09-21 17:43:57 -0400

    remove unwanted content

--- src/records/articles.ts
+++
@@ -1,8 +0,0 @@
-// Most recent should be first
-export default [
-    'dune-part-two-review',
-    'placeholder-article4',
-    'placeholder-article3',
-    'placeholder-article2',
-    'placeholder-article1',
-]
